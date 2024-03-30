893675b74739960778196e1c22c36f0a212d8b9f
Vardan Farsiyan <farsiyan.vartan@yandex.ru>
2024-03-30 19:08:29 +0300

    Update HealthController.java

--- src/main/java/ru/otus/DockerHomework/controller/HealthController.java
+++ src/main/java/ru/otus/DockerHomework/controller/HealthController.java
@@ -6,11 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Map;
+
 @RestController
 @RequestMapping(path = "/health")
 public class HealthController {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public void health() {
+    public Map<String, String> health() {
+        return Collections.singletonMap("status", "OK");
     }
 }
